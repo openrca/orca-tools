e261d325d96dda215ab5b514271f3553c56dbf4e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-09-07 18:57:44 +0200

    Bootstrap Python package
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as requirements:
+        return requirements.read().splitlines()
+
+
+setup(
+    name='orca_tools',
+    version='0.1.0',
+    long_description=readme(),
+    url='ttps://github.com/openrca/orca-tools',
+    author='OpenRCA',
+    license='Apache License 2.0',
+    install_requires=get_requirements(),
+    packages=find_packages(),
+    zip_safe=False)
